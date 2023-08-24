8a2302372b8af142c6a30e88b368333868279038
Omar Baldi <omar.baldi@maersk.com>
2023-08-24 18:02:58 +0200

    Refactoring by creating separate type definition for state update with callback

--- src/composables/useState.ts
+++ src/composables/useState.ts
@@ -1,13 +1,17 @@
 import { Ref, shallowRef } from 'vue';
 
-function isStateFunction<T>(s: T | ((prevValue: T) => T)): s is (prevValue: T) => T {
+type CallbackStateFunction<T> = (prevValue: T) => T;
+
+function isStateFunction<T>(
+  s: T | CallbackStateFunction<T>
+): s is CallbackStateFunction<T> {
   return typeof s === 'function';
 }
 
 export const useState = <T>(state: T) => {
   const value = shallowRef<T>(state);
 
-  const updateValue = (updatedValue: T | ((prevValue: T) => T)) => {
+  const updateValue = (updatedValue: T | CallbackStateFunction<T>) => {
     if (isStateFunction(updatedValue)) {
       value.value = (updatedValue as (prevValue: T) => T)(value.value);
     } else {
